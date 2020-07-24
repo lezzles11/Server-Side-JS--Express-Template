6addb935d46b1c91a1185637f3418e0ce401fc49
lesleyc@bu.edu <lesleyc@bu.edu>
2020-07-24 14:14:27 +0800

    all routes work

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const path = require("path");
-const Router = express.Router();
+const hbs = require("express-handlebars");
+
+const fetch = require("node-fetch");
 var cors = require("cors");
-const https = require("https");
-const fs = require("fs");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "hbs");
 
 // express.static() specifies the path where the files are found
 // This makes sure that you get all assets from the public folder
@@ -16,6 +25,37 @@ app.use((req, res, next) => {
   console.log("\nMETHOD: " + req.method);
   console.log("\nPATH: " + req.path + "\n");
   next();
+});
+
+// Render the home page, with all the notes included already
+app.get("/", (req, res) => {
+  fetch("http://localhost:3001/notes")
+    .then((response) =>
+      response.json().then((json) => {
+        res.render("home", {
+          articles: json,
+        });
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// Render the page, add_note
+app.get("/add_note", (req, res) => {
+  res.render("add_note");
+});
+
+// Render the page, edit note
+app.get("/edit_note/:id", (req, res) => {
+  fetch(`http://localhost:3001/notes/${req.params.id}`).then((response) => {
+    response.json().then((json) => {
+      res.render("edit_note", {
+        articles: json,
+      });
+    });
+  });
 });
 
 app.get("/notes", cors(), (req, res) => {
@@ -29,19 +69,42 @@ app.get("/notes/:id", cors(), (req, res) => {
   res.send(`Get one note with ${req.params.id}`);
 });
 
-app.post("/notes", cors(), (req, res) => {
+app.post("/api/add_note", cors(), (req, res) => {
   console.log("Posting a note");
-  res.send(`Posting note`);
+  fetch("http://localhost:3001/notes", {
+    method: "POST",
+    body: JSON.stringify(req.body),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((response) => {
+    res.status(200).send();
+  });
 });
 
-app.put("/notes/:id", cors(), (req, res) => {
+app.patch("/api/edit_note/:id", cors(), (req, res) => {
   console.log("Editing a note");
-  res.send(`Changing note with ${req.params.id}`);
-});
+  const id = req.params.id;
 
-app.delete("/notes/:id", cors(), (req, res) => {
+  fetch(`http://localhost:3001/notes/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(req.body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    res.status(200).send();
+  });
+});
+//DELETE
+app.delete("/api/delete/:id", cors(), (req, res) => {
   console.log("Deleting a note");
-  res.send(`Deleting notes with ${req.params.id}`);
+  const id = req.params.id;
+  fetch(`http://localhost:3001/notes/${id}`, {
+    method: "DELETE",
+  }).then((response) => {
+    res.status(200).send();
+  });
 });
 
 app.use((req, res, next) => {
